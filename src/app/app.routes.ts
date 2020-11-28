064283db97d35ed2ab9d33cea647595b8556b205
Chris-AM <c.aranguizm@outlook.com>
2020-11-28 01:43:17 -0300

    pathMatch home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,10 @@ import { HomeComponent } from './components/home/home.component';
 
 
 const APP_ROUTES: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'heroes', component: HeroesComponent },
   { path: 'about', component: AboutComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
